a2bc1e78a396731cc3ce9ef225fbd1e37fdbcec1
Burak Gazi Chetin <burakgazic@yandex.com>
2022-01-22 18:03:42 +0300

    Add the Music script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -xe
+youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=E3LeZNlI0Xg
+youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=lVDCLFIM0L8&list=OLAK5uy_kpn22DVWUKOq23w1iVhDKwiA7iITwHH3A &
+youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=VT24CnRX8Zw&list=OLAK5uy_kpn22DVWUKOq23w1iVhDKwiA7iITwHH3A&index=6 &
+youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=Hgdqq-E-OFE &
+youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=XpwIrZ2vg8s &
+youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=8sy_sPVFWjQ &
+youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=8caXPWdYW9g &
+youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=WJW_ldC7sUA &
+youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=0T9guE-LyHg &
+youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=pG4OVL2ek7Y &
+youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=KFiScXBMR7E &
